8c5be2970eb852fa255d55c401d81fa4b504bc5e
StefanYYC <stefanspeter26@gmail.com>
2022-05-05 23:04:30 +0200

    add recent posts / similar posts block

--- services/index.js
+++ services/index.js
@@ -33,7 +33,50 @@ export const getPosts = async () => {
     }
   `
 
-    const result = await request(graphqlAPI, query)
-    return result.postsConnection.edges;
-
+  const result = await request(graphqlAPI, query)
+  return result.postsConnection.edges
 }
+
+export const getRecentPosts = async () => {
+  const query = gql`
+    query GetPostDetails() {
+      posts(
+        orderBy: createdAt_ASC
+        last: 3
+        ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          slug
+        }
+    }
+  `
+  const result = await request(graphqlAPI, query)
+  return result.posts
+}
+
+export const getSimilarPosts = async () => {
+  // we pass parameters and specify their type (for categories it's an array of string)
+  // explications where: don't display the current articles (with slug_not) AND display articles that are in the same categories at current post
+  const query = gql`
+    query GetPostDetails($slug: String!, $categories: [String!]) {
+      posts(
+        where: { slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+        last: 3
+        ) {
+          title
+          featuredImage {
+            url
+          }
+          createdAt
+          slug
+        }
+    }
+  `
+  const result = await request(graphqlAPI, query)
+  return result.postsConnection.edges
+}
+
+
